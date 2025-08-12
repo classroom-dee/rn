1f538f46aad462633996455e23b59da747eefb4b
Dongwon Henry Seon <tunacome@gmail.com>
2025-08-12 02:44:43 +0000

    Exercise 10.7 scroll

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import AppBarTab from './AppBarTab';
@@ -7,21 +7,31 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.navBarBG,
-    flexDirection: "row",
-    columnGap: 10
   },
   tabs: {
     color: theme.colors.textWhite,
     fontSize: 20,
     fontWeight: 'bold'
+  },
+  scollview: {
+    flexDirection: "row",
+    columnGap: 10,
+    alignItems: 'center',
+
   }
 });
 // console.log(`the height of the stat bar in this env is: ${Constants.statusBarHeight}`)
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab style={styles.tabs} title="Repositories" to="/" />
-      <AppBarTab style={styles.tabs} title="Sign in" to="/signin" />
+      <ScrollView 
+        contentContainerStyle={styles.scollview} 
+        showsHorizontalScrollIndicator={false}
+        horizontal
+      >
+        <AppBarTab style={styles.tabs} title="Repositories" to="/" />
+        <AppBarTab style={styles.tabs} title="Sign in" to="/signin" />
+      </ScrollView>
     </View>
   )
 };
